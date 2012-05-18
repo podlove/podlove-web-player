5ccaa28ff5070119974d164bd7b50d136aca0569
Thomas Lohninger <lohninger.thomas@gmail.com>
2012-05-18 05:53:37 +0200

    beta deep linking
    everything besides start-stop deep links is working and organized in
    fine encapsulated manner.

--- podlove-web-player/podlove-chapters.js
+++ podlove-web-player/podlove-chapters.js
@@ -2,9 +2,16 @@ var PODLOVE = PODLOVE || {};
 PODLOVE.playercount = 0;
 
 PODLOVE.chapters = function (playerId) {
+    // look for deeplinks, if no other player has been loaded
     if (!PODLOVE.playercount) {
-        //console.log('startpos:' + PODLOVE.chapters.startpos);
-        if (PODLOVE.chapters.startpos = PODLOVE.chapters.parseTimecode((window.location.href.match(/#(\d\d:\d\d:\d\d\.\d\d\d)/) || [])[1])) {
+        //console.log('startpos:' + PODLOVE.ref_deep_links);
+        PODLOVE.ref_deep_links = window.location.href.match(/(\d\d:\d\d:\d\d\.\d\d\d)/) || [];
+        PODLOVE.ref_deep_links.splice(1,1);
+        $(PODLOVE.ref_deep_links).each(function (i, e) {
+            PODLOVE.ref_deep_links[i] = PODLOVE.chapters.parseTimecode(e);
+        });
+
+        if (PODLOVE.ref_deep_links.length) {
             $('#' + playerId).attr('preload', 'auto');
             //console.log ('preload of player ' + $('#' + playerId).attr('preload'));
         }
@@ -13,36 +20,65 @@ PODLOVE.chapters = function (playerId) {
     MediaElement(playerId, {
         success: function (player) {
             PODLOVE.playercount++;
-            PODLOVE.chapters.addBehaviour(playerId, player);
+            PODLOVE.chapters.addBehaviour_chapter(playerId, player);
+            PODLOVE.chapters.addBehaviour_deep_linking(playerId, player);
         }
     });
 };
 
-PODLOVE.chapters.addBehaviour = function (playerId, player) {
+PODLOVE.chapters.addBehaviour_deep_linking = function (playerId, player) {
+    var skip_to_linked_time = function (e) {
+            if (PODLOVE.playercount === 1) {
+                if (PODLOVE.ref_deep_links.length  === 1) {
+                    player.setCurrentTime(PODLOVE.ref_deep_links[0]);
+                    PODLOVE.ref_deep_links = null;
+                } else {
+                    if (PODLOVE.ref_deep_links[0]) {
+                        player.setCurrentTime(PODLOVE.ref_deep_links[0]);
+                        PODLOVE.ref_deep_links[0] = null;
+                    } else if (PODLOVE.ref_deep_links[1]) {
+                        player.pause();
+                        PODLOVE.ref_deep_links[0] = null;
+                    }
+                }
+                
+            }
+        }
+      , address_current_time = function (e) {
+            if (PODLOVE.playercount === 1) {
+                history.pushState(null, null, '#' + PODLOVE.chapters.generateTimecode(player.currentTime));
+            }
+      }
+
+    if (PODLOVE.playercount === 1) {
+        if (PODLOVE.ref_deep_links.length) {
+            player.addEventListener('play', skip_to_linked_time , false);
+            player.addEventListener('timeupdate', skip_to_linked_time , false);
+        } else {
+            player.addEventListener('pause', address_current_time, false);
+            player.addEventListener('seeked', address_current_time, false);
+        }
+        jQuery('table[rel=' + playerId + ']').on('click', 'a', function () {
+            history.pushState(null, null, '#' + PODLOVE.chapters.generateTimecode(time) + '-' + PODLOVE.chapters.generateTimecode(jQuery(this).find('span').data('end')));
+        });
+    }
+
+
+}
+
+PODLOVE.chapters.addBehaviour_chapter = function (playerId, player) {
     var list = jQuery('table[rel=' + playerId + ']')
         .show()
         .on('click', 'a', function () {
             var time = jQuery(this).find('span').data('start');
             player.setCurrentTime(time);
             player.play();
-            history.pushState(null, null, '#' + PODLOVE.chapters.generateTimecode(time) + '-' + PODLOVE.chapters.generateTimecode(jQuery(this).find('span').data('end')));
             return false;
         })
-     , skip_to_linked_time = function (e) {
 
-            if (PODLOVE.chapters.startpos && PODLOVE.playercount < 2) {
-	        player.setCurrentTime(PODLOVE.chapters.startpos);
-                PODLOVE.chapters.startpos = null;
-            }
-        };
-
-    if (PODLOVE.chapters.startpos && PODLOVE.playercount < 2) {
-        player.addEventListener('play', skip_to_linked_time , false);
-        player.addEventListener('timeupdate', skip_to_linked_time , false);
-    }
 
     player.addEventListener('timeupdate', function (e) {
-        //console.log('timeupdate ' + window.location.href + ' - ' + PODLOVE.chapters.startpos + ' --- ' + player.buffered.end(0));
+        //console.log('timeupdate ' + window.location.href + ' - ' + PODLOVE.ref_deep_links + ' --- ' + player.buffered.end(0));
         // update the chapter list when the data is loaded
         list.find('span').each(function (i) {
             var span = jQuery(this),
@@ -61,11 +97,13 @@ PODLOVE.chapters.addBehaviour = function (playerId, player) {
                     .removeClass('active');
                 row.addClass('active');
                 // we don't update the address if a selectedchapter is selected and we have not yet played through it
-                if (PODLOVE.playercount < 2 && !PODLOVE.chapters.startpos) {
+/*
+                if (PODLOVE.playercount < 2 && !PODLOVE.ref_deep_links) {
                     if (!(tmpTimecode = window.location.href.match(/#(\d\d:\d\d:\d\d\.\d\d\d)-(\d\d:\d\d:\d\d\.\d\d\d)/)) || (tmpTimecode[2] && PODLOVE.chapters.parseTimecode(tmpTimecode[2]) <= startTime)) {
                         history.pushState(null, null, '#' + PODLOVE.chapters.generateTimecode(startTime));
                     }
                 }
+*/
             }
             if (!isEnabled && isBuffered) {
                 span.data('enabled', '1').wrap('<a href="#"></a>');
