131154417878da4f7c3b0d23f7b46b49ffd1b884
Thomas Lohninger <lohninger.thomas@gmail.com>
2012-05-18 14:53:55 +0200

    finish
    cleanup & final version without stop times

--- podlove-web-player/podlove-chapters.js
+++ podlove-web-player/podlove-chapters.js
@@ -2,10 +2,9 @@ var PODLOVE = PODLOVE || {};
 PODLOVE.playercount = 0;
 
 PODLOVE.chapters = function (playerId) {
-    // look for deeplinks, if no other player has been loaded
+    // parse deeplinks
     if (!PODLOVE.playercount) {
-        //console.log('startpos:' + PODLOVE.ref_deep_links);
-        PODLOVE.ref_deep_links = window.location.href.match(/(\d\d:\d\d:\d\d\.\d\d\d)/) || [];
+        PODLOVE.ref_deep_links = window.location.href.match(/((\d\d:)?\d\d:\d\d(\.\d\d\d)?)/) || [];
         PODLOVE.ref_deep_links.splice(1,1);
         $(PODLOVE.ref_deep_links).each(function (i, e) {
             PODLOVE.ref_deep_links[i] = PODLOVE.chapters.parseTimecode(e);
@@ -13,7 +12,6 @@ PODLOVE.chapters = function (playerId) {
 
         if (PODLOVE.ref_deep_links.length) {
             $('#' + playerId).attr('preload', 'auto');
-            //console.log ('preload of player ' + $('#' + playerId).attr('preload'));
         }
     }
 
@@ -25,46 +23,6 @@ PODLOVE.chapters = function (playerId) {
         }
     });
 };
-
-PODLOVE.chapters.addBehaviour_deep_linking = function (playerId, player) {
-    var skip_to_linked_time = function (e) {
-            if (PODLOVE.playercount === 1) {
-                if (PODLOVE.ref_deep_links.length  === 1) {
-                    player.setCurrentTime(PODLOVE.ref_deep_links[0]);
-                    PODLOVE.ref_deep_links = [];
-                } else {
-                    if (PODLOVE.ref_deep_links[0]) {
-                        player.setCurrentTime(PODLOVE.ref_deep_links[0]);
-                        PODLOVE.ref_deep_links[0] = null;
-                    } else if (PODLOVE.ref_deep_links[1]) {
-                        player.pause();
-                        PODLOVE.ref_deep_links[0] = null;
-                    }
-                }
-                
-            }
-        }
-      , address_current_time = function (e) {
-            if (PODLOVE.playercount === 1) {
-                history.pushState(null, null, '#' + PODLOVE.chapters.generateTimecode(player.currentTime));
-            }
-      }
-
-    if (PODLOVE.playercount === 1) {
-        if (PODLOVE.ref_deep_links.length) {
-            player.addEventListener('play', skip_to_linked_time , false);
-            player.addEventListener('timeupdate', skip_to_linked_time , false);
-        } else {
-            player.addEventListener('pause', address_current_time, false);
-            player.addEventListener('seeked', address_current_time, false);
-        }
-        jQuery('table[rel=' + playerId + ']').on('click', 'a', function () {
-            history.pushState(null, null, '#' + PODLOVE.chapters.generateTimecode(jQuery(this).find('span').data('start')) + '-' + PODLOVE.chapters.generateTimecode(jQuery(this).find('span').data('end')));
-        });
-    }
-
-
-}
 
 PODLOVE.chapters.addBehaviour_chapter = function (playerId, player) {
     var list = jQuery('table[rel=' + playerId + ']')
@@ -78,7 +36,6 @@ PODLOVE.chapters.addBehaviour_chapter = function (playerId, player) {
 
 
     player.addEventListener('timeupdate', function (e) {
-        //console.log('timeupdate ' + window.location.href + ' - ' + PODLOVE.ref_deep_links + ' --- ' + player.buffered.end(0));
         // update the chapter list when the data is loaded
         list.find('span').each(function (i) {
             var span = jQuery(this),
@@ -96,20 +53,45 @@ PODLOVE.chapters.addBehaviour_chapter = function (playerId, player) {
                     .find('tr.active')
                     .removeClass('active');
                 row.addClass('active');
-                // we don't update the address if a selectedchapter is selected and we have not yet played through it
-/*
-                if (PODLOVE.playercount < 2 && !PODLOVE.ref_deep_links) {
-                    if (!(tmpTimecode = window.location.href.match(/#(\d\d:\d\d:\d\d\.\d\d\d)-(\d\d:\d\d:\d\d\.\d\d\d)/)) || (tmpTimecode[2] && PODLOVE.chapters.parseTimecode(tmpTimecode[2]) <= startTime)) {
-                        history.pushState(null, null, '#' + PODLOVE.chapters.generateTimecode(startTime));
-                    }
-                }
-*/
             }
             if (!isEnabled && isBuffered) {
                 span.data('enabled', '1').wrap('<a href="#"></a>');
             }
         });
     }, false);
+};
+
+
+/** 
+ * deeplinking: skipt to referenced time position & write current times into adress
+ * @param playerId string
+ * @param player object
+ */
+PODLOVE.chapters.addBehaviour_deep_linking = function (playerId, player) {
+    var skip_to_linked_time = function (e) {
+            if (PODLOVE.playercount === 1 && PODLOVE.ref_deep_links.length) {
+                player.setCurrentTime(PODLOVE.ref_deep_links[0]);
+                PODLOVE.ref_deep_links = [];
+            }
+        }
+      , address_current_time = function (e) {
+            if (PODLOVE.playercount === 1 && !PODLOVE.ref_deep_links.length) {
+                history.pushState(null, null, '#' + PODLOVE.chapters.generateTimecode(player.currentTime));
+            }
+      }
+
+    if (PODLOVE.playercount === 1) {
+        if (PODLOVE.ref_deep_links.length) {
+            player.addEventListener('play',       skip_to_linked_time , false);
+            player.addEventListener('timeupdate', skip_to_linked_time , false);
+        }
+        player.addEventListener('pause',  address_current_time, false);
+        player.addEventListener('seeked', address_current_time, false);
+
+        jQuery('table[rel=' + playerId + ']').on('click', 'a', function () {
+            history.pushState(null, null, '#' + PODLOVE.chapters.generateTimecode(jQuery(this).find('span').data('start')));
+        });
+    }
 };
 
 /**
@@ -124,9 +106,8 @@ PODLOVE.chapters.generateTimecode = function (sec) {
                }
       , tcode;
     tcode = prim(Math.floor(sec/60/60)) + ':' + prim(Math.floor(sec/60)%60) + ':' + prim(Math.floor(sec%60)%60) + '.' + prim(((sec-Math.floor(sec)) + '').substring(2,5) || '000', true);
-    //console.log('generateTimecode (' + sec + ') ==> ' + tcode);
     return tcode;
-}
+};
 
 /**
  * parses time code into seconds
@@ -135,13 +116,12 @@ PODLOVE.chapters.generateTimecode = function (sec) {
  **/
 PODLOVE.chapters.parseTimecode = function (tcode) {
     var parts;
-    if ((parts = (tcode || '').match(/(\d\d):(\d\d):(\d\d).(\d\d\d)/)) && parts.length === 5) {
-        parts = parseInt (parts[1]) * 60 * 60 + 
-                parseInt (parts[2]) * 60 + 
-                parseInt (parts[3]) +
-                parseFloat('0.' + parts[4]);
-       //console.log ('parseTimecode (' + tcode + ') ==> ' + parts);
+    if ((parts = (tcode || '').match(/((\d\d:)?(\d\d):(\d\d)(\.\d\d\d)?)/)) && parts.length === 6) {
+        parts = (parts[2] ? parseInt (parts[2].substring(0,2)) * 60 * 60 : 0) + 
+                parseInt (parts[3]) * 60 + 
+                parseInt (parts[4]) +
+                (parts[5] ? parseFloat('0' + parts[5]) : 0);
        return parts;
     }
-}
+};
 
