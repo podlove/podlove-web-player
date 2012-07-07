9d1cfc48ee007e64ebfa16f3c31caae1685e5b3f
Paul Golmann <master@pje.li>
2012-07-07 12:11:52 +0200

    .js: New Features!
    - Using HTML5 pushState instead of setting location.hash.
    - Even more smaller functions for better structure…
    - Generate time periods as described in
    http://www.w3.org/TR/media-frags/#fragment-dimensions

--- podlove-web-player/podlove-chapters.js
+++ podlove-web-player/podlove-chapters.js
@@ -7,7 +7,10 @@ var PODLOVE = PODLOVE || {};
 		// Count Players on site
 		playerCount = 0,
 		// Timecode as described in http://podlove.org/deep-link/
-		timecodeRegExp = /(\d\d:)?(\d\d):(\d\d)(\.\d\d\d)?/;
+		// and http://www.w3.org/TR/media-frags/#fragment-dimensions
+		timecodeRegExp = /(\d\d:)?(\d\d):(\d\d)(\.\d\d\d)?(,(\d\d:)?(\d\d):(\d\d)(\.\d\d\d)?)?/,
+		// check if to use html5 pushState
+		usePushState = !!(window.history && history.pushState);
 
 	/**
 	 * return number as string lefthand filled with zeros
@@ -21,28 +24,38 @@ var PODLOVE = PODLOVE || {};
 	}
 
 	/**
-	 * returns seconds in deep-linking time format
-	 * @param seconds number
+	 * accepts array with start and end time in seconds
+	 * returns timecode in deep-linking format
+	 * @param times array
 	 * @return string
 	 **/
-	function generateTimecode(seconds) {
-		var timecode, hours, milliseconds;
+	function generateTimecode(times) {
+		var startTime, endTime;
 
-		// prevent negative values from player
-		if (seconds <= 0) {
-			return '00:00';
+		function generatePart(seconds) {
+			var part, hours, milliseconds;
+			// prevent negative values from player
+			if (!seconds || seconds <= 0) {
+				return '00:00';
+			}
+
+			// required (minutes : seconds)
+			part = zeroFill(Math.floor(seconds / 60) % 60, 2) + ':' +
+					zeroFill(Math.floor(seconds % 60) % 60, 2);
+
+			hours = zeroFill(Math.floor(seconds / 60 / 60), 2);
+			hours = hours === '00' ? '' : hours + ':';
+			milliseconds = zeroFill(Math.floor(seconds % 1 * 1000), 3);
+			milliseconds = milliseconds === '000' ? '' : '.' + milliseconds;
+
+			return hours + part + milliseconds;
 		}
 
-		// required (minutes : seconds)
-		timecode = zeroFill(Math.floor(seconds / 60) % 60, 2) + ':' +
-				zeroFill(Math.floor(seconds % 60) % 60, 2);
+		if (times[1] > 0 && times[1] < 9999999 && times[0] < times[1]) {
+			return generatePart(times[0]) + ',' + generatePart(times[1]);
+		}
 
-		hours = zeroFill(Math.floor(seconds / 60 / 60), 2);
-		hours = hours === '00' ? '' : hours + ':';
-		milliseconds = zeroFill(Math.floor(seconds % 1 * 1000), 3);
-		milliseconds = milliseconds === '000' ? '' : '.' + milliseconds;
-
-		return hours + timecode + milliseconds;
+		return generatePart(times[0]);
 	}
 
 	/**
@@ -51,24 +64,96 @@ var PODLOVE = PODLOVE || {};
 	 * @return number
 	 **/
 	function parseTimecode(timecode) {
-		var parts, seconds = 0;
+		var parts, startTime = 0, endTime = 0;
 
 		if (timecode) {
 			parts = timecode.match(timecodeRegExp);
-			if (parts && parts.length === 5) {
-				// hours
-				seconds += parts[1] ? parseInt(parts[1], 10) * 60 * 60 : 0;
-				// minutes
-				seconds += parseInt(parts[2], 10) * 60;
-				// seconds
-				seconds += parseInt(parts[3], 10);
-				// milliseconds
-				seconds += parts[4] ? parseFloat(parts[4]) : 0;
 
-				return Math.max(seconds, 0);
+			if (parts && parts.length === 10) {
+				// hours
+				startTime += parts[1] ? parseInt(parts[1], 10) * 60 * 60 : 0;
+				// minutes
+				startTime += parseInt(parts[2], 10) * 60;
+				// seconds
+				startTime += parseInt(parts[3], 10);
+				// milliseconds
+				startTime += parts[4] ? parseFloat(parts[4]) : 0;
+				// no negative time
+				startTime = Math.max(startTime, 0);
+
+				// if there only a startTime but no endTime
+				if (parts[5] === undefined) {
+					return [startTime, false];
+				}
+
+				// hours
+				endTime += parts[6] ? parseInt(parts[6], 10) * 60 * 60 : 0;
+				// minutes
+				endTime += parseInt(parts[7], 10) * 60;
+				// seconds
+				endTime += parseInt(parts[8], 10);
+				// milliseconds
+				endTime += parts[9] ? parseFloat(parts[9]) : 0;
+				// no negative time
+				endTime = Math.max(endTime, 0);
+
+				return (endTime > startTime) ? [startTime, endTime] : [startTime, false];
 			}
 		}
 		return false;
+	}
+
+	function setFragmentURL(fragment) {
+		var url;
+
+		if (usePushState) {
+			url = window.location.href.split('#')[0] + '#' + fragment;
+			window.history.pushState(null, null, url);
+		} else {
+			window.location.hash = fragment;
+		}
+	}
+
+	// update the chapter list when the data is loaded
+	function updateChapterMarks(player, marks) {
+		marks.each(function () {
+			var deepLink,
+				span       = $(this),
+				startTime  = span.data('start'),
+				endTime    = span.data('end'),
+				isEnabled  = span.data('enabled') === '1',
+				isBuffered = player.buffered.end(0) > startTime,
+				isActive   = player.currentTime > startTime - 0.3 &&
+						player.currentTime <= endTime;
+
+			if (isActive) {
+				span.closest('tr')
+					.addClass('active')
+					.siblings().removeClass('active');
+			}
+			if (!isEnabled && isBuffered) {
+				deepLink = '#t=' + generateTimecode([startTime, endTime]);
+				span
+					.data('enabled', '1')
+					.wrap('<a href="' + deepLink + '"></a>');
+			}
+		});
+	}
+
+	function skipToLinkedTime(e) {
+		console.log('skipToLinkedTime');
+		if (deepLink !== false && playerCount === 1) {
+			e.data.player.setCurrentTime(deepLink[0]);
+			deepLink = false;
+		}
+	}
+
+	function addressCurrentTime(e) {
+		var fragment;
+		if (deepLink === false && playerCount === 1) {
+			fragment = 't=' + generateTimecode([e.data.player.currentTime]);
+			setFragmentURL(fragment);
+		}
 	}
 
 	PODLOVE.web_player = function (playerId) {
@@ -111,66 +196,43 @@ var PODLOVE = PODLOVE || {};
 			.delegate('a', 'click', function (e) {
 				e.preventDefault();
 
-				var time = $(this).find('span').data('start');
-				player.setCurrentTime(time);
+				var startTime = $(this).find('span').data('start'),
+					endTime = $(this).find('span').data('end');
+
+				// Basic Chapter Mark function (without deeplinking)
+				player.setCurrentTime(startTime);
 				if (player.pluginType !== 'flash') {
 					player.play();
 				}
+
+				// If there is only one player also set deepLink
 				if (playerCount === 1) {
-					deepLink = time;
+					setFragmentURL('t=' + generateTimecode([startTime, endTime]));
 				}
 			});
 
-		function skipToLinkedTime() {
-			if (deepLink !== false && playerCount === 1) {
-				player.setCurrentTime(deepLink);
-				deepLink = false;
-			}
-		}
-		function addressCurrentTime() {
-			var timecode;
-			if (deepLink === false && playerCount === 1) {
-				timecode = generateTimecode(player.currentTime);
-				window.location.hash = '#' + timecode;
-			}
-		}
-
 		// wait for the player or you'll get DOM EXCEPTIONS
 		jqPlayer.bind('canplay', function () {
+			// add Deeplink Behavior if there is only one player on the site
 			if (playerCount === 1) {
 				jqPlayer.bind({
 					play: skipToLinkedTime,
 					timeupdate: skipToLinkedTime,
-					pause: addressCurrentTime,
-					seeked: addressCurrentTime
-				});
+					pause: addressCurrentTime
+					// disabled 'cause it overrides chapter clicks
+					// seeked: addressCurrentTime
+				}, {player: player});
+				// handle browser history navigation
+				if (usePushState) {
+					window.onpopstate = function () {
+						// parse deeplink
+						deepLink = parseTimecode(window.location.href);
+					};
+				}
 			}
+			// always update Chaptermarks though
 			jqPlayer.bind('timeupdate', function () {
-				// update the chapter list when the data is loaded
-				marks.each(function () {
-					var deepLink,
-						span       = $(this),
-						startTime  = span.data('start'),
-						endTime    = span.data('end'),
-						isEnabled  = span.data('enabled') === '1',
-						isBuffered = player.buffered.end(0) > startTime,
-						isActive   = player.currentTime > startTime - 0.3 &&
-									 player.currentTime <= endTime;
-
-					if (isActive) {
-						span.closest('tr')
-							.addClass('active')
-							.siblings().removeClass('active');
-					}
-					if (!isEnabled && isBuffered) {
-						deepLink = '#' + generateTimecode(startTime) +
-								(endTime && endTime < 9999999 ? '-' +
-								generateTimecode(endTime) : '');
-						span
-							.data('enabled', '1')
-							.wrap('<a href="' + deepLink + '"></a>');
-					}
-				});
+				updateChapterMarks(player, marks);
 			});
 		});
 	};
