2a72a3be08821c32f40ad18dc692597beec6d117
Gerrit van Aaken <gerrit@praegnanz.de>
2012-07-19 10:12:37 +0200

    setting version number to 1.0.9

--- podlove-web-player/podlove-web-player.php
+++ podlove-web-player/podlove-web-player.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package PodloveWebPlayer
- * @version 1.0.8
+ * @version 1.0.9
  */
 
 /*
@@ -9,7 +9,7 @@ Plugin Name: Podlove Web Player
 Plugin URI: http://podlove.org/podlove-web-player/
 Description: Video and audio plugin for WordPress built on the MediaElement.js HTML5 media player library.
 Author: Gerrit van Aaken and others
-Version: 1.0.8
+Version: 1.0.9
 Author URI: http://praegnanz.de
 License: GPLv3, MIT
 */
@@ -98,7 +98,7 @@ if (!get_option('pwp_script_on_demand')) {
 			// the scripts
 			wp_enqueue_script('mediaelementjs-scripts', PODLOVEWEBPLAYER_MEJS_DIR . 'mediaelement-and-player.min.js', array('jquery'), '2.9.1', false);
 			wp_enqueue_script('ba-hashchange', PODLOVEWEBPLAYER_DIR . 'libs/jquery.ba-hashchange.min.js', array('jquery'), '1.3.0', false);
-			wp_enqueue_script('podlove-web-player', PODLOVEWEBPLAYER_DIR . 'podlove-web-player.js', array('jquery', 'mediaelementjs-scripts'), '1.0.8', false);
+			wp_enqueue_script('podlove-web-player', PODLOVEWEBPLAYER_DIR . 'podlove-web-player.js', array('jquery', 'mediaelementjs-scripts'), '1.0.9', false);
 		}
 	}
 	add_action('wp_print_scripts', 'podlove_pwp_add_scripts');
