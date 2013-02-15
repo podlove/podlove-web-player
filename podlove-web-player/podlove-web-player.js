19727169084ea92d72bf2b5d6c60c5a58926a128
gerritvanaaken <gerrit@praegnanz.de>
2013-02-15 15:23:56 +0100

    even more simplified js

--- podlove-web-player/podlove-web-player.js
+++ podlove-web-player/podlove-web-player.js
@@ -9,182 +9,7 @@
 		// and http://www.w3.org/TR/media-frags/#fragment-dimensions
 		timecodeRegExp = /(\d+:)?(\d+):(\d+)(\.\d+)?([,-](\d+:)?(\d+):(\d+)(\.\d+)?)?/;
 
-	/**
-	 * return number as string lefthand filled with zeros
-	 * @param number number
-	 * @param width number
-	 * @return string
-	 **/
-	function zeroFill(number, width) {
-		width -= number.toString().length;
-		return width > 0 ? new Array(width + 1).join('0') + number : number + '';
-	}
 
-	/**
-	 * accepts array with start and end time in seconds
-	 * returns timecode in deep-linking format
-	 * @param times array
-	 * @return string
-	 **/
-	function generateTimecode(times) {
-		function generatePart(seconds) {
-			var part, hours, milliseconds;
-			// prevent negative values from player
-			if (!seconds || seconds <= 0) {
-				return '00:00';
-			}
-
-			// required (minutes : seconds)
-			part = zeroFill(Math.floor(seconds / 60) % 60, 2) + ':' +
-					zeroFill(Math.floor(seconds % 60) % 60, 2);
-
-			hours = zeroFill(Math.floor(seconds / 60 / 60), 2);
-			hours = hours === '00' ? '' : hours + ':';
-			milliseconds = zeroFill(Math.floor(seconds % 1 * 1000), 3);
-			milliseconds = milliseconds === '000' ? '' : '.' + milliseconds;
-
-			return hours + part + milliseconds;
-		}
-
-		if (times[1] > 0 && times[1] < 9999999 && times[0] < times[1]) {
-			return generatePart(times[0]) + ',' + generatePart(times[1]);
-		}
-
-		return generatePart(times[0]);
-	}
-
-	/**
-	 * parses time code into seconds
-	 * @param string timecode
-	 * @return number
-	 **/
-	function parseTimecode(timecode) {
-		var parts, startTime = 0, endTime = 0;
-
-		if (timecode) {
-			parts = timecode.match(timecodeRegExp);
-
-			if (parts && parts.length === 10) {
-				// hours
-				startTime += parts[1] ? parseInt(parts[1], 10) * 60 * 60 : 0;
-				// minutes
-				startTime += parseInt(parts[2], 10) * 60;
-				// seconds
-				startTime += parseInt(parts[3], 10);
-				// milliseconds
-				startTime += parts[4] ? parseFloat(parts[4]) : 0;
-				// no negative time
-				startTime = Math.max(startTime, 0);
-
-				// if there only a startTime but no endTime
-				if (parts[5] === undefined) {
-					return [startTime, false];
-				}
-
-				// hours
-				endTime += parts[6] ? parseInt(parts[6], 10) * 60 * 60 : 0;
-				// minutes
-				endTime += parseInt(parts[7], 10) * 60;
-				// seconds
-				endTime += parseInt(parts[8], 10);
-				// milliseconds
-				endTime += parts[9] ? parseFloat(parts[9]) : 0;
-				// no negative time
-				endTime = Math.max(endTime, 0);
-
-				return (endTime > startTime) ? [startTime, endTime] : [startTime, false];
-			}
-		}
-		return false;
-	}
-
-	function turnHighlightOff () { 
-		$('.highlight').removeClass('highlight');
-	}
-
-	function checkCurrentURL() {
-		var deepLink;
-		deepLink = parseTimecode(window.location.href);
-		if (deepLink !== false) {
-			startAtTime = deepLink[0];
-			stopAtTime = deepLink[1];
-		}
-	}
-
-	function setFragmentURL(fragment) {
-		var url;
-		window.location.hash = fragment;
-	}
-
-	// update the chapter list when the data is loaded
-	function updateChapterMarks(player, marks) {
-		var doLinkMarks = marks.closest('table').hasClass('linked');
-
-		marks.each(function () {
-			var deepLink,
-				mark       = $(this),
-				startTime  = mark.data('start'),
-				endTime    = mark.data('end'),
-				isEnabled  = mark.data('enabled'),
-				// isBuffered = player.buffered.end(0) > startTime,
-				isActive   = player.currentTime > startTime - 0.3 &&
-						player.currentTime <= endTime;
-
-			// prevent timing errors
-			if (player.buffered.length > 0) {
-			  var isBuffered = player.buffered.end(0) > startTime;
-			}
-
-			if (isActive) {
-				mark
-					.addClass('active')
-					.siblings().removeClass('active');
-			}
-			if (!isEnabled && isBuffered) {
-				deepLink = '#t=' + generateTimecode([startTime, endTime]);
-
-				$(mark).data('enabled', true).addClass('loaded').find('a[rel=player]').removeClass('disabled');
-				/*
-				if (doLinkMarks && mark.find('a').length === 0) {
-					mark.find('td.title')
-						.wrapInner('<a href="' + deepLink + '" />');
-				}
-				*/
-			}
-		});
-	}
-
-	function checkTime(e) {
-		if (players.length > 1) { return; }
-		var player = e.data.player;
-		if (startAtTime !== false && 
-			//Kinda hackish: Make sure that the timejump is at least 1 second (fix for OGG/Firefox)
-			(typeof player.lastCheck === "undefined" || 
-			Math.abs(startAtTime - player.lastCheck) > 1)) {
-			player.setCurrentTime(startAtTime);
-			player.lastCheck = startAtTime;
-			startAtTime = false;
-		}
-		if (stopAtTime !== false && player.currentTime >= stopAtTime) {
-			player.pause();
-			stopAtTime = false;
-		}
-	}
-
-	function addressCurrentTime(e) {
-		var fragment;
-		/* Why did we need that? It prevented firefox from generating fragments after pause
-		if (players.length === 1 &&
-				stopAtTime === false &&
-				startAtTime === false) {
-		*/
-		if (players.length === 1) {
-			fragment = 't=' + generateTimecode([e.data.player.currentTime]);
-			setFragmentURL(fragment);
-		}
-	}
-
-	/* --------------------- Build actual player ---- */
 
 	$.fn.podlovewebplayer = function(options) {
 		var player = this[0];
@@ -715,4 +540,171 @@
 
 		});
 	};
+
+
+
+
+
+
+	/**
+	 * return number as string lefthand filled with zeros
+	 * @param number number
+	 * @param width number
+	 * @return string
+	 **/
+	function zeroFill(number, width) {
+		width -= number.toString().length;
+		return width > 0 ? new Array(width + 1).join('0') + number : number + '';
+	}
+
+
+	/**
+	 * accepts array with start and end time in seconds
+	 * returns timecode in deep-linking format
+	 * @param times array
+	 * @return string
+	 **/
+	function generateTimecode(times) {
+		function generatePart(seconds) {
+			var part, hours, milliseconds;
+			// prevent negative values from player
+			if (!seconds || seconds <= 0) {
+				return '00:00';
+			}
+
+			// required (minutes : seconds)
+			part = zeroFill(Math.floor(seconds / 60) % 60, 2) + ':' +
+					zeroFill(Math.floor(seconds % 60) % 60, 2);
+
+			hours = zeroFill(Math.floor(seconds / 60 / 60), 2);
+			hours = hours === '00' ? '' : hours + ':';
+			milliseconds = zeroFill(Math.floor(seconds % 1 * 1000), 3);
+			milliseconds = milliseconds === '000' ? '' : '.' + milliseconds;
+
+			return hours + part + milliseconds;
+		}
+
+		if (times[1] > 0 && times[1] < 9999999 && times[0] < times[1]) {
+			return generatePart(times[0]) + ',' + generatePart(times[1]);
+		}
+
+		return generatePart(times[0]);
+	}
+
+	/**
+	 * parses time code into seconds
+	 * @param string timecode
+	 * @return number
+	 **/
+	function parseTimecode(timecode) {
+		var parts, startTime = 0, endTime = 0;
+
+		if (timecode) {
+			parts = timecode.match(timecodeRegExp);
+
+			if (parts && parts.length === 10) {
+				// hours
+				startTime += parts[1] ? parseInt(parts[1], 10) * 60 * 60 : 0;
+				// minutes
+				startTime += parseInt(parts[2], 10) * 60;
+				// seconds
+				startTime += parseInt(parts[3], 10);
+				// milliseconds
+				startTime += parts[4] ? parseFloat(parts[4]) : 0;
+				// no negative time
+				startTime = Math.max(startTime, 0);
+
+				// if there only a startTime but no endTime
+				if (parts[5] === undefined) {
+					return [startTime, false];
+				}
+
+				// hours
+				endTime += parts[6] ? parseInt(parts[6], 10) * 60 * 60 : 0;
+				// minutes
+				endTime += parseInt(parts[7], 10) * 60;
+				// seconds
+				endTime += parseInt(parts[8], 10);
+				// milliseconds
+				endTime += parts[9] ? parseFloat(parts[9]) : 0;
+				// no negative time
+				endTime = Math.max(endTime, 0);
+
+				return (endTime > startTime) ? [startTime, endTime] : [startTime, false];
+			}
+		}
+		return false;
+	}
+
+	function checkCurrentURL() {
+		var deepLink;
+		deepLink = parseTimecode(window.location.href);
+		if (deepLink !== false) {
+			startAtTime = deepLink[0];
+			stopAtTime = deepLink[1];
+		}
+	}
+
+	function setFragmentURL(fragment) {
+		var url;
+		window.location.hash = fragment;
+	}
+
+	// update the chapter list when the data is loaded
+	function updateChapterMarks(player, marks) {
+		var doLinkMarks = marks.closest('table').hasClass('linked');
+
+		marks.each(function () {
+			var deepLink,
+				mark       = $(this),
+				startTime  = mark.data('start'),
+				endTime    = mark.data('end'),
+				isEnabled  = mark.data('enabled'),
+				// isBuffered = player.buffered.end(0) > startTime,
+				isActive   = player.currentTime > startTime - 0.3 &&
+						player.currentTime <= endTime;
+
+			// prevent timing errors
+			if (player.buffered.length > 0) {
+			  var isBuffered = player.buffered.end(0) > startTime;
+			}
+
+			if (isActive) {
+				mark
+					.addClass('active')
+					.siblings().removeClass('active');
+			}
+			if (!isEnabled && isBuffered) {
+				deepLink = '#t=' + generateTimecode([startTime, endTime]);
+
+				$(mark).data('enabled', true).addClass('loaded').find('a[rel=player]').removeClass('disabled');
+			}
+		});
+	}
+
+	function checkTime(e) {
+		if (players.length > 1) { return; }
+		var player = e.data.player;
+		if (startAtTime !== false && 
+			//Kinda hackish: Make sure that the timejump is at least 1 second (fix for OGG/Firefox)
+			(typeof player.lastCheck === "undefined" || 
+			Math.abs(startAtTime - player.lastCheck) > 1)) {
+			player.setCurrentTime(startAtTime);
+			player.lastCheck = startAtTime;
+			startAtTime = false;
+		}
+		if (stopAtTime !== false && player.currentTime >= stopAtTime) {
+			player.pause();
+			stopAtTime = false;
+		}
+	}
+
+	function addressCurrentTime(e) {
+		var fragment;
+		if (players.length === 1) {
+			fragment = 't=' + generateTimecode([e.data.player.currentTime]);
+			setFragmentURL(fragment);
+		}
+	}
+	
 }(jQuery));
