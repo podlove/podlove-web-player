a74861776160fcd79b7978b0b8a483b56fd2e9c7
Simon Waldherr <git@simon.waldherr.eu>
2013-06-15 18:28:13 +0200

    add link to podlove using podcasts list and to the podlove donation page

--- podlove-web-player/settings.php
+++ podlove-web-player/settings.php
@@ -239,11 +239,15 @@ function podlovewebplayer_buttons_share() {
 function podlovewebplayer_info() {
 	$scriptname = explode('/wp-admin', $_SERVER["SCRIPT_FILENAME"]);
 	$dirname    = explode('/wp-content', dirname(__FILE__));
-	print '<p>This is <strong>Version 2.0.9</strong> of the <strong>Podlove Web Player</strong>.<br>
+	print '<p>This is <strong>Version 2.1.0</strong> of the <strong>Podlove Web Player</strong>.<br>
 	The <strong>Including file</strong> is: <code>wp-admin'.$scriptname[1].'</code><br>
 	The <strong>PWP-directory</strong> is: <code>wp-content'.$dirname[1].'</code></p>
 	<p>Want to contribute? Found a bug? Need some help? <br/>you can found our github repo/page at
 	<a href="https://github.com/podlove/podlove-web-player">github.com/podlove/podlove-web-player</a></p>
+	<p>You can\'t help with code? Ok, you can help with money, too. Go to 
+	<a href="http://podlove.org/donations/">podlove.org/donations/</a> to donate to the Podlove Project.</p>
+	<p>We have <a href="https://github.com/podlove/podlove.github.com/wiki">lists with Podlove Publisher 
+	and Podlove Web Player using podcasts</a>, feel free to enter your podcast.</p>
 	<p>If you found a bug, please tell us your WP- and PWP- (and PPP- if you use PPP) Version. <br/>Also your 
 	Browser version, your PHP version and the URL of your Podcast can help us, find the bug.</p>';
 }
