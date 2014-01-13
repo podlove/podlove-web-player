a0865f34f2308422b392c9b0b1071cd739dc8ebb
Juri Leino <github@line-o.de>
2014-01-13 18:14:27 +0100

    add moderator script for embedded players

--- podlove-web-player/podlove-webplayer-moderator.js
+++ podlove-web-player/podlove-webplayer-moderator.js
@@ -1,45 +1,84 @@
-;(function($) {
-    var players = [];
+;
+(function ($) {
+    var players = {};
+
     $.fn.podlovewebplayer = function (options) {
-        return this.replaceWith(function() {
+        return this.replaceWith(function () {
             var $element = $(this);
-            var frame = $('<iframe>', {
-                src : $element.data('podlove-web-player-source'),
-                height: 300,
-                width: 800,
+            var $frame = $('<iframe>', {
+                src: $element.data('podlove-web-player-source'),
+                height: $element.data('podlove-web-player-height') || 300,
+                width: $element.data('podlove-web-player-width') || 800,
                 className: 'podlove-webplayer-frame',
-                css:{
-                    border:"none",
-                    overflow:"hidden"
+                css: {
+                    border: "none",
+                    overflow: "hidden"
                 }
             });
+            var frame = $frame.get(0);
 
-            $(window).on('message', function(event){
-                var orig = event.originalEvent;
-
-                if( frame.get(0).contentWindow != orig.source) {
-                    if( orig.data.action == 'play'){
-                        frame.get(0).contentWindow.postMessage({
-                            action: 'pause'
-                        },'*');
-                    }
-                    return;
-                }
-
-                if( orig.data.action == 'ready' && orig.source == frame.get(0).contentWindow ){
-                }
-
-                if( orig.data.action == 'resize'){
-                    frame.height(orig.data.arg);
-                }
-            });
+            $(window).on('message', handleMessage);
 
             // register player frame
-            players.push(frame);
+            console.debug('registered player with src=', frame.src);
+            players[frame.src] = {
+                frame: $frame,
+                state: -1
+            };
 
             return frame;
         });
+    };
+
+    function handleMessage (event) {
+        var orig = event.originalEvent;
+
+        // discard hash - it changes along with the time media is played
+        var src = orig.source.location.href;
+        var hash = orig.source.location.hash;
+        var id = src.substr(0, src.length - hash.length);
+        var player = players[id];
+
+        console.debug(orig.data.action, orig.data.arg);
+
+        if (player == null) {
+            console.warn('no player found with src=', id);
+            return;
+        }
+
+        console.debug('received action:', orig.data.action, 'from', id, 'with:', orig.data.arg);
+
+        if (orig.data.action == 'ready') {
+            player.state = 0;
+        }
+
+        if (orig.data.action == 'play') {
+            if (player.state === 1) return;
+            player.state = 1;
+            pausePlayersExceptOne(id);
+        }
+
+        if (orig.data.action == 'pause') {
+            if (player.state === 0) return;
+            player.state = 0;
+        }
+
+        if (orig.data.action == 'resize') {
+            player.frame.height(orig.data.arg);
+        }
+
     }
 
+    function pausePlayersExceptOne(currentPlayerId) {
+        var playerData, playerId, message = {action: 'pause'};
+        for (playerId in players) {
+            console.debug('probe', playerId);
+            if (playerId === currentPlayerId || !players.hasOwnProperty(playerId))
+                return;
+            console.debug('pausing', playerId);
+            playerData = players[playerId];
+            playerData.frame.get(0).contentWindow.postMessage(message, playerId);
+        }
+    }
 })(jQuery);
 
