aa8a5428a9ead48349939b433b807848bc624f31
Juri Leino <github@line-o.de>
2014-01-13 19:16:08 +0100

    use native event handling in moderator

--- podlove-web-player/podlove-webplayer-moderator.js
+++ podlove-web-player/podlove-webplayer-moderator.js
@@ -1,8 +1,54 @@
 ;
 (function ($) {
-    var players = {};
-
     $.fn.podlovewebplayer = function (options) {
+        var players = {};
+        function handleMessage (event) {
+            // discard hash - it changes along with the time media is played
+            var src = event.source.location.href;
+            var hash = event.source.location.hash;
+            var id = src.substr(0, src.length - hash.length);
+            var player = players[id];
+
+            console.debug(event.data.action, event.data.arg);
+
+            if (player == null) {
+                console.warn('no player found with src=', id);
+                return;
+            }
+
+            console.debug('received', event.data.action, 'from', id, 'with', event.data.arg);
+
+            if (event.data.action == 'ready') {
+                player.state = 0;
+            }
+
+            if (event.data.action == 'play') {
+                if (player.state === 1) return;
+                player.state = 1;
+                pausePlayersExceptOne(id);
+            }
+
+            if (event.data.action == 'pause') {
+                if (player.state === 0) return;
+                player.state = 0;
+            }
+
+            if (event.data.action == 'resize') {
+                player.frame.height(event.data.arg);
+            }
+        }
+
+        function pausePlayersExceptOne(currentPlayerId) {
+            var playerData, playerId, message = {action: 'pause'};
+            for (playerId in players) {
+                if (playerId === currentPlayerId || !players.hasOwnProperty(playerId))
+                    continue;
+                console.debug('pausing', playerId);
+                playerData = players[playerId];
+                playerData.frame.get(0).contentWindow.postMessage(message, playerId);
+            }
+        }
+        
         return this.replaceWith(function () {
             var $element = $(this);
             var $frame = $('<iframe>', {
@@ -17,7 +63,8 @@
             });
             var frame = $frame.get(0);
 
-            $(window).on('message', handleMessage);
+            //$(window).on('message', handleMessage);
+            window.addEventListener("message", handleMessage, false);
 
             // register player frame
             console.debug('registered player with src=', frame.src);
@@ -26,59 +73,8 @@
                 state: -1
             };
 
-            return frame;
+            return $frame;
         });
     };
-
-    function handleMessage (event) {
-        var orig = event.originalEvent;
-
-        // discard hash - it changes along with the time media is played
-        var src = orig.source.location.href;
-        var hash = orig.source.location.hash;
-        var id = src.substr(0, src.length - hash.length);
-        var player = players[id];
-
-        console.debug(orig.data.action, orig.data.arg);
-
-        if (player == null) {
-            console.warn('no player found with src=', id);
-            return;
-        }
-
-        console.debug('received action:', orig.data.action, 'from', id, 'with:', orig.data.arg);
-
-        if (orig.data.action == 'ready') {
-            player.state = 0;
-        }
-
-        if (orig.data.action == 'play') {
-            if (player.state === 1) return;
-            player.state = 1;
-            pausePlayersExceptOne(id);
-        }
-
-        if (orig.data.action == 'pause') {
-            if (player.state === 0) return;
-            player.state = 0;
-        }
-
-        if (orig.data.action == 'resize') {
-            player.frame.height(orig.data.arg);
-        }
-
-    }
-
-    function pausePlayersExceptOne(currentPlayerId) {
-        var playerData, playerId, message = {action: 'pause'};
-        for (playerId in players) {
-            console.debug('probe', playerId);
-            if (playerId === currentPlayerId || !players.hasOwnProperty(playerId))
-                return;
-            console.debug('pausing', playerId);
-            playerData = players[playerId];
-            playerData.frame.get(0).contentWindow.postMessage(message, playerId);
-        }
-    }
 })(jQuery);
 
