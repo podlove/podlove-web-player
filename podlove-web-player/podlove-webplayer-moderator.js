a8bf95501c44dc1db98c9a4cd1acff1cd9697627
Juri Leino <github@line-o.de>
2014-01-15 10:49:08 +0100

    restructured and improved webplayer moderator
    * register `messageHandler` only once
    * declare iframe dimension boundaries and defaults explicitly at the
    beginning
    * moved function `handleMessage` to outer scope of jQuery extension
    (enclosing immediate function)
    * discard message if no action is included in `handleMessage`
    * function `pausePlayersExceptOne` sends pause command only if the
    embedded player is currently playing (status is `1`)
    * add docblocks to functions
    * add function `getDimensions` that sanitizes the given value according
    to min, max and default value

--- podlove-web-player/podlove-webplayer-moderator.js
+++ podlove-web-player/podlove-webplayer-moderator.js
@@ -1,60 +1,127 @@
 ;
 (function ($) {
+    var IFRAME_HEIGHT_DEFAULT = 300,
+        IFRAME_HEIGHT_MIN = 100,
+        IFRAME_HEIGHT_MAX = 3000,
+        IFRAME_WIDTH_DEFAULT = 800,
+        IFRAME_WIDTH_MIN = 400,
+        IFRAME_WIDTH_MAX = 2000,
+        players = {};
+
+    /**
+     * decide what to do with a received message
+     * @param {jQuery.Event} event
+     */
+    function handleMessage (event) {
+        // discard hash - it changes along with the time media is played
+        var originalEvent = event.originalEvent,
+            data = originalEvent.data,
+            action = data.action,
+            argumentObject = data.arg,
+            eventLocation = originalEvent.source.location,
+            href = eventLocation.href,
+            hash = eventLocation.hash,
+            id = href.substr(0, href.length - hash.length),
+            player = players[id];
+
+        //console.debug('received message', action, argumentObject);
+
+        if (player == null) {
+            //console.debug('no player found with src=', id);
+            return;
+        }
+
+        if (action == null || argumentObject == null) {
+            //console.debug('no action or data was given');
+            return;
+        }
+
+        //console.debug('received', event.data.action, 'from', id, 'with', event.data.arg);
+
+        if (action == 'ready' || action == 'pause') {
+            player.state = 0;
+        }
+
+        if (action == 'play') {
+            player.state = 1;
+            pausePlayersExceptOne(id);
+        }
+
+        if (action == 'resize') {
+            player.frame.height(getPlayerHeight(argumentObject));
+        }
+    }
+
+    /**
+     * Pause all registered players except the one with the given ID
+     * @param {String} currentPlayerId
+     */
+    function pausePlayersExceptOne(currentPlayerId) {
+        var playerData, playerId, message = {action: 'pause'};
+        for (playerId in players) {
+            if (playerId === currentPlayerId || !players.hasOwnProperty(playerId)) {
+                continue;
+            }
+            playerData = players[playerId];
+            if (playerData.state === 0) { continue; } // nothing to do, it is already paused
+
+            playerData.frame.get(0).contentWindow.postMessage(message, playerId);
+        }
+    }
+
+    /**
+     * Sanitize player height
+     * @param {Number} height
+     * @returns {Number}
+     */
+    function getPlayerHeight (height) {
+        return getDimension(height, IFRAME_HEIGHT_MIN, IFRAME_HEIGHT_MAX, IFRAME_HEIGHT_DEFAULT);
+    }
+
+    /**
+     * Sanitize player width
+     * @param {Number} width
+     * @returns {Number}
+     */
+    function getPlayerWidth (width) {
+        return getDimension(width, IFRAME_WIDTH_MIN, IFRAME_WIDTH_MAX, IFRAME_WIDTH_DEFAULT);
+    }
+
+    /**
+     * Sanitize player dimension
+     * @param {Number} value
+     * @param {Number} min
+     * @param {Number} max
+     * @param {Number} defaultValue
+     * @returns {Number} new dimension
+     */
+    function getDimension (value, min, max, defaultValue) {
+        if (isNaN(value)) {
+            console.warn('Dimension', value, 'is not a number.');
+            return defaultValue;
+        }
+        if (value < min || value > max) {
+            console.warn('Dimension', value, 'out of bounds.');
+            return defaultValue;
+        }
+        return value;
+    }
+
+    // receive messages from embedded players
+    $(window).on('message', handleMessage);
+
+    /**
+     * Replace selection of nodes with embedded podlove webplayers and register them internally
+     * @param {Object} options
+     * @returns {jQuery} jQuery extended HTMLIFrameElement
+     */
     $.fn.podlovewebplayer = function (options) {
-        var players = {};
-        function handleMessage (event) {
-            // discard hash - it changes along with the time media is played
-            var src = event.source.location.href;
-            var hash = event.source.location.hash;
-            var id = src.substr(0, src.length - hash.length);
-            var player = players[id];
-
-            console.debug(event.data.action, event.data.arg);
-
-            if (player == null) {
-                console.warn('no player found with src=', id);
-                return;
-            }
-
-            console.debug('received', event.data.action, 'from', id, 'with', event.data.arg);
-
-            if (event.data.action == 'ready') {
-                player.state = 0;
-            }
-
-            if (event.data.action == 'play') {
-                if (player.state === 1) return;
-                player.state = 1;
-                pausePlayersExceptOne(id);
-            }
-
-            if (event.data.action == 'pause') {
-                if (player.state === 0) return;
-                player.state = 0;
-            }
-
-            if (event.data.action == 'resize') {
-                player.frame.height(event.data.arg);
-            }
-        }
-
-        function pausePlayersExceptOne(currentPlayerId) {
-            var playerData, playerId, message = {action: 'pause'};
-            for (playerId in players) {
-                if (playerId === currentPlayerId || !players.hasOwnProperty(playerId))
-                    continue;
-                console.debug('pausing', playerId);
-                playerData = players[playerId];
-                playerData.frame.get(0).contentWindow.postMessage(message, playerId);
-            }
-        }
-        
         return this.replaceWith(function () {
             var $element = $(this);
             var $frame = $('<iframe>', {
                 src: $element.data('podlove-web-player-source'),
-                height: $element.data('podlove-web-player-height') || 300,
-                width: $element.data('podlove-web-player-width') || 800,
+                height: getPlayerHeight($element.data('podlove-web-player-height')),
+                width: getPlayerWidth($element.data('podlove-web-player-width')),
                 className: 'podlove-webplayer-frame',
                 css: {
                     border: "none",
@@ -63,15 +130,12 @@
             });
             var frame = $frame.get(0);
 
-            //$(window).on('message', handleMessage);
-            window.addEventListener("message", handleMessage, false);
-
             // register player frame
-            console.debug('registered player with src=', frame.src);
             players[frame.src] = {
                 frame: $frame,
                 state: -1
             };
+            console.info('registered player with src=', frame.src);
 
             return $frame;
         });
