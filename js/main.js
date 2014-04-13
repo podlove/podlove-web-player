18b6f6258bd4a58161e69c6ac34f21dbab1fde6c
Juri Leino <github@line-o.de>
2014-04-13 19:50:49 +0200

    further restructuring and cleanup
    Move or remove the last unnecessary files from project root.
    Rename podlove-webplayer-moderator.js to podlove-web-moderator.js.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*
  * ===========================================
- * Podlove Web Player v2.0.18
+ * Podlove Web Player v2.1.0-alpha
  * Licensed under The BSD 2-Clause License
  * http://opensource.org/licenses/BSD-2-Clause
  * ===========================================
@@ -14,5 +14,25 @@
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+'use strict';
 
+require('./../libs/mediaelement/build/mediaelement-and-player.js');
 
+// FIXME put in compat mode module
+if (typeof String.prototype.trim !== 'function') {
+  String.prototype.trim = function () {
+    "use strict";
+    return this.replace(/^\s+|\s+$/g, '');
+  };
+}
+
+var pwp = {
+  tc: require('./timecode'),
+  players: require('./player').players,
+  embed: require('./embed')
+};
+
+//FIXME without embed animations are fluent
+//pwp.embed.init($, pwp.players);
+
+module.exports = pwp;
