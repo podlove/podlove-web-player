07044e3cc7a749227c4ea4f0956c8f722a5ce7f2
Plutonik <plutonik@gmx.net>
2014-07-12 15:16:07 +0200

    Refactor module info.js
    - Indent code in a more readable way
    - Add date object to render a publication date
    - Add div in info-tab-footer with social links

--- src/js/modules/info.js
+++ src/js/modules/info.js
@@ -21,48 +21,43 @@ function createInfoTab(params) {
     active: !!params.summaryVisible
   });
 
+  var date = new Date(params.publicationDate);
+
   infoTab.createSection(
-      '<section><h2>'
-      + params.title
-      + '</h2><p>Veröffentlicht: ' + params.publicationDate + '</p><h4>'
-      + params.subtitle
-      + '</h4><p>'
-      + params.summary
-      + '</p><p>Dauer: '
-      + params.duration
-      + '</p><p>Weblink dieser Episode:<br><a href="'
-      + params.permalink
-      + '">'
-      + params.permalink
-      + '</a></p></section>');
+    '<h2>' + params.title + '</h2>'
+    +'<p>Veröffentlicht: ' + date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear() + '</p>'
+    +'<em>' + params.subtitle + '</em>'
+    +'<p>' + params.summary + '</p>'
+    +'<p>Dauer: ' + params.duration + '</p>'
+    +'<p>Weblink dieser Episode:<br>'
+      +'<a href="' + params.permalink + '">' + params.permalink + '</a>'
+    +'</p>');
 
   infoTab.createAside(
-    '<h2>'
-    + params.show.title
-    + '</h2><div class="coverart"><img class="coverimg" src="'
-    + params.poster
-    + '" data-img="'
-    + params.poster
-    + '" alt="Poster Image"></div><form><input type="submit" value="'
-    + params.show.title
-    + ' abonnieren" aria-label="'
-    + params.show.title
-    + 'abonnieren"></form><p>'
-    + params.show.subtitle
-    + '</p><p>Weblink dieser Show: <br><a href="'
-    + params.show.url
-    + '">'
-    + params.show.url
-    + '</a></p>');
+    '<h2>'+ params.show.title+ '</h2>'
+    +'<div class="image-container"><img class="" src="' + params.poster + '" data-img="' + params.poster + '" alt="Poster Image"></div>'
+      +'<button class="button-subscribe">'
+        +'<span class="showtitle-label">' + params.show.title + '</span>'
+        +'<span class="submit-label">Abonnieren</span>'
+      +'</button>'
+    +'<p>'+ params.show.subtitle + '</p>'
+    +'<p>Weblink dieser Show: <br><a href="' + params.show.url + '">' + params.show.url + '</a></p>');
 
   infoTab.createFooter(
-      '<p>The show "'
-    + params.show.title
-    + '" is licenced under<br><a href="'
-    + params.license.url
-    + '">'
-    + params.license.name
-    + '</a></p>');
+    '<div class="social-links">'
+      +'<h3>Bleib in Verbindung</h3>'
+      +'<ul>'
+        +'<li><a class="twitter" title="Twitter" href="https://twitter.com/"><img src="twitter-128.png"><span>Twitter</span></a></li>'
+        +'<li><a class="flattr" title="Flattr" href="https://twitter.com/"><img src="flattr-128.png"><span>Flattr</span></a></li>'
+        +'<li><a class="google" title="Google+" href="https://twitter.com/"><img src="googleplus-128.png"><span>Google+</span></a></li>'
+        +'<li><a class="facebook" title="Facebook" href="https://twitter.com/"><img src="facebook-128.png"><span>Facebook</span></a></li>'
+        +'<li><a class="adn" title="App.net" href="https://twitter.com/"><img src="adn-128.png"><span>App.net</span></a></li>'
+        +'<li><a class="email" title="E-mail" href="https://twitter.com/"><img src="email-128.png"><span>E-mail</span></a></li>'
+      +'</ul>'
+    +'</div>'
+    +'<p>The show "' + params.show.title + '" is licenced under<br>'
+      +'<a href="' + params.license.url + '">' + params.license.name + '</a>'
+    +'</p>');
 
   return infoTab;
 }
