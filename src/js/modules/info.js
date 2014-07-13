c940cdd6d0f5e311fc809e11caa9cb4589a838d4
Juri Leino <github@line-o.de>
2014-07-13 20:33:36 +0200

    refactor info module
    Render social links, license information and subscribe button only when
    possible.
    
    Define social link types with class name, title and image.
    
    Add helper functions for each subtask.
    
    * createEpisodeInfo
    * createPosterImage
    * createSubscribeButton
    * createShowInfo
    * createSocialLink
    * createSocialInfo
    * createSocialAndLicenseInfo

--- src/js/modules/info.js
+++ src/js/modules/info.js
@@ -1,7 +1,113 @@
-var Tab = require('../tab');
+var Tab = require('../tab')
+  , types = {
+    'twitter': {
+      'className': 'twitter',
+      'title': 'Twitter',
+      'image': 'img/twitter-128.png'
+    },
+    'flattr': {
+      'className': 'flattr',
+      'title': 'Flattr',
+      'image': 'img/flattr-128.png'
+    },
+    'facebook': {
+      'className': 'facebook',
+      'title': 'Facebook',
+      'image': 'img/facebook-128.png'
+    },
+    'adn': {
+      'className': 'adn',
+      'title': 'Twitter',
+      'image': 'img/twitter-128.png'
+    },
+    'email': {
+      'className': 'email',
+      'title': 'E-Mail',
+      'image': 'img/email-128.png'
+    },
+    'googleplus': {
+      'className': 'google',
+      'title': 'Google+',
+      'image': 'img/googleplus-128.png'
+    }
+  }
+  ;
 
-function Info(params) {
-  this.tab = createInfoTab(params);
+function createEpisodeInfo(tab, params) {
+  var date = new Date(params.publicationDate);
+
+  tab.createSection(
+    '<h2>' + params.title + '</h2>' +
+    '<p>Published: ' + date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear() + '</p>' +
+    '<em>' + params.subtitle + '</em>' +
+    '<p>' + params.summary + '</p>' +
+    '<p>Duration: ' + params.duration + '</p>' +
+    '<p>' +
+      'Permalink for this episode:<br>' +
+      '<a href="' + params.permalink + '">' + params.permalink + '</a>' +
+    '</p>'
+  );
+}
+
+function createPosterImage(poster) {
+  if (!poster) {
+    return '';
+  }
+  return '<div class="image-container">' +
+    '<img src="' + poster + '" data-img="' + poster + '" alt="Poster Image">' +
+    '</div>';
+}
+
+function createSubscribeButton(params) {
+  if (!params.subscribeButton) {
+    return '';
+  }
+  return '<button class="button-subscribe">' +
+      '<span class="showtitle-label">' + params.show.title + '</span>' +
+      '<span class="submit-label">' + params.subscribeButton + '</span>' +
+    '</button>';
+}
+
+function createShowInfo (tab, params) {
+  tab.createAside(
+    '<h2>'+ params.show.title+ '</h2>' +
+    createPosterImage(params.poster) +
+    createSubscribeButton(params) +
+    '<p>' + params.show.subtitle + '</p>' +
+    '<p>Link to the show:<br>' +
+      '<a href="' + params.show.url + '">' + params.show.url + '</a></p>'
+  );
+}
+
+function createSocialLink(type, url) {
+  if (!(type in types)) {
+    console.warn('createSocialLink called with unknown type', type);
+    return '';
+  }
+  return '<li><a class="' + types[type].className + '" title="' + types[type].title + '" href="' + url + '">' +
+      '<img src="' + types[type].image + '"><span>' + types[type].title + '</span></a></li>';
+}
+
+function createSocialInfo(social) {
+  if (!social) {
+    return '';
+  }
+  return '<div class="social-links">' +
+      '<h3>Stay in touch</h3>' +
+      '<ul>' + social.links.map(createSocialLink).join('') + '</ul>' +
+    '</div>';
+}
+
+function createSocialAndLicenseInfo (tab, params) {
+  if (!params.license && !params.social) {
+    return;
+  }
+  tab.createFooter(
+    createSocialInfo(params.social) +
+    '<p>The show "' + params.show.title + '" is licenced under<br>' +
+      '<a href="' + params.license.url + '">' + params.license.name + '</a>' +
+    '</p>'
+  );
 }
 
 /**
@@ -14,52 +120,26 @@ function createInfoTab(params) {
     return null;
   }
   var infoTab = new Tab({
-    icon:"pwp-icon-info-circle",
-    title:"More information about this",
+    icon: 'pwp-icon-info-circle',
+    title: 'More information about this',
     headline: 'Info',
-    name:'info',
+    name: 'info',
     active: !!params.summaryVisible
   });
 
-  var date = new Date(params.publicationDate);
-
-  infoTab.createSection(
-    '<h2>' + params.title + '</h2>'
-    +'<p>Veröffentlicht: ' + date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear() + '</p>'
-    +'<em>' + params.subtitle + '</em>'
-    +'<p>' + params.summary + '</p>'
-    +'<p>Dauer: ' + params.duration + '</p>'
-    +'<p>Weblink dieser Episode:<br>'
-      +'<a href="' + params.permalink + '">' + params.permalink + '</a>'
-    +'</p>');
-
-  infoTab.createAside(
-    '<h2>'+ params.show.title+ '</h2>'
-    +'<div class="image-container"><img class="" src="' + params.poster + '" data-img="' + params.poster + '" alt="Poster Image"></div>'
-      +'<button class="button-subscribe">'
-        +'<span class="showtitle-label">' + params.show.title + '</span>'
-        +'<span class="submit-label">Abonnieren</span>'
-      +'</button>'
-    +'<p>'+ params.show.subtitle + '</p>'
-    +'<p>Weblink dieser Show: <br><a href="' + params.show.url + '">' + params.show.url + '</a></p>');
-
-  infoTab.createFooter(
-    '<div class="social-links">'
-      +'<h3>Bleib in Verbindung</h3>'
-      +'<ul>'
-        +'<li><a class="twitter" title="Twitter" href="https://twitter.com/"><img src="twitter-128.png"><span>Twitter</span></a></li>'
-        +'<li><a class="flattr" title="Flattr" href="https://twitter.com/"><img src="flattr-128.png"><span>Flattr</span></a></li>'
-        +'<li><a class="google" title="Google+" href="https://twitter.com/"><img src="googleplus-128.png"><span>Google+</span></a></li>'
-        +'<li><a class="facebook" title="Facebook" href="https://twitter.com/"><img src="facebook-128.png"><span>Facebook</span></a></li>'
-        +'<li><a class="adn" title="App.net" href="https://twitter.com/"><img src="adn-128.png"><span>App.net</span></a></li>'
-        +'<li><a class="email" title="E-mail" href="https://twitter.com/"><img src="email-128.png"><span>E-mail</span></a></li>'
-      +'</ul>'
-    +'</div>'
-    +'<p>The show "' + params.show.title + '" is licenced under<br>'
-      +'<a href="' + params.license.url + '">' + params.license.name + '</a>'
-    +'</p>');
-
   return infoTab;
+}
+
+/**
+ *
+ * @param {object} params
+ * @constructor
+ */
+function Info(params) {
+  this.tab = createInfoTab(params);
+  createEpisodeInfo(this.tab, params);
+  createShowInfo(this.tab, params);
+  createSocialAndLicenseInfo(this.tab, params);
 }
 
 module.exports = Info;
