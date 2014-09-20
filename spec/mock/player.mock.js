a6fa7ac724fa438f5b03cee91730c4cba77d955e
Juri Leino <github@line-o.de>
2014-09-20 02:24:28 +0200

    add player mock for testing

---
+++ spec/mock/player.mock.js
@@ -0,0 +1,11 @@
+module.exports = {
+  time: 0,
+  HAVE_ENOUGH_DATA: 1,
+  readyState: 1,
+  setCurrentTime: function (time) {
+    this.time = time;
+  },
+  play: function () {},
+  pause: function () {},
+  stop: function () {}
+};
