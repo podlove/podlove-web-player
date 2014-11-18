b9f7a54cb579d3a2244ebe035f2d2696e81d3a41
Juri Leino <github@line-o.de>
2014-11-19 00:54:33 +0100

    minor readability improvement in SocialNetwork

--- src/js/social-network.js
+++ src/js/social-network.js
@@ -91,6 +91,7 @@ SocialNetwork.prototype.getButton = function (options) {
 module.exports = SocialNetwork;
 
 function createButton (options) {
-  return $('<a class="pwp-contrast-' + options.icon + '" target="_blank" href="' + options.url + '" title="' + options.title + '">' +
-    '<i class="pwp-' + options.icon + '"></i></a><span>' + options.title + '</span>');
+  return $('<a class="pwp-contrast-' + options.icon + '" target="_blank" href="' + options.url + '" ' +
+    'title="' + options.title + '"><i class="pwp-' + options.icon + '"></i></a>' +
+    '<span>' + options.title + '</span>');
 }
