cf323d4864c552007205c2eb2cf05a2e4c497825
Juri Leino <github@line-o.de>
2014-11-22 04:24:26 +0100

    download tab with fallback poster image

--- src/js/modules/downloads.js
+++ src/js/modules/downloads.js
@@ -34,6 +34,32 @@ function createOption(listElement) {
   return '<option>' + listElement.assetTitle + ' ' + formatSize(listElement.size) + '</option>'
 }
 
+function getPosterImage(params) {
+  var defaultPoster = '/img/icon-podlove-subscribe-600.png';
+  var defaultClass = 'default-poster';
+
+  var poster = defaultPoster;
+  if (params.show.poster) {
+    poster = params.show.poster;
+    defaultClass = '';
+  }
+  if (params.poster) {
+    poster = params.poster;
+    defaultClass = '';
+  }
+
+  return '<img class="poster-image ' + defaultClass + '" src="' + poster + '" data-img="' + poster + '" ' +
+    'alt="Poster Image">';
+}
+
+function getPublicationDate(rawDate) {
+  if (!rawDate) {
+    return '';
+  }
+  var date = new Date(rawDate);
+  return '<p>Published: ' + date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear() + '</p>';
+}
+
 /**
  *
  * @param {object} params
@@ -43,7 +69,6 @@ Downloads.prototype.createDownloadTab = function (params) {
   if ((!params.downloads && !params.sources) || params.hidedownloadbutton === true) {
     return null;
   }
-  var date = new Date(params.publicationDate);
   var downloadTab = new Tab({
       icon: "pwp-download",
       title: "Show/hide download bar",
@@ -54,12 +79,12 @@ Downloads.prototype.createDownloadTab = function (params) {
   var $tabContent = downloadTab.createMainContent('<div class="download">' +
       '<div class="poster-wrapper">' +
         '<div class="download download-overlay"></div>' +
-        '<img class="poster-image" src="' + params.poster + '" data-img="' + params.poster + '" alt="Poster Image">' +
+         getPosterImage(params) +
       '</div>' +
     '</div>' +
     '<div class="download">' +
       '<h2>' + params.title + '</h2>' +
-      '<p>Published: ' + date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear() + '</p>' +
+      getPublicationDate(params.publicationDate) +
       '<p>Duration: ' + timeCode.fromTimeStamp(params.duration) + '</p>' +
     '</div>'
     );
