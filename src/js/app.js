e1f7a8dc81dd2d77204dc42f8e04fb10c666e4f7
Juri Leino <github@line-o.de>
2014-11-22 02:44:59 +0100

    update version in app module

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 /**!
  * ===========================================
- * Podlove Web Player v2.1.0-alpha
+ * Podlove Web Player v3.0.0-alpha
  * Licensed under The BSD 2-Clause License
  * http://opensource.org/licenses/BSD-2-Clause
  * ===========================================
