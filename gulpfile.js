aca18b7e4c64bf886c3f96d8460696e2a12ff85a
Plutonik <plutonik@gmx.net>
2015-01-07 20:19:55 +0100

    build separate stylesheet for each theme

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('tdd', function (done) {
 
 // Styles
 gulp.task('styles', function() {
-  return gulp.src(source + 'sass/podlove-web-player.scss')
+  return gulp.src(source + 'sass/pwp-*.scss')
     .pipe(compass({
       config_file: './config.rb',
       style: 'expanded',
