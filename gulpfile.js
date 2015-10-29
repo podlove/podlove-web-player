1305447277f7c9479e95df4e5d5043883def7ce1
Juri Leino <github@line-o.de>
2015-10-29 19:53:37 +0100

    include source comments in compiled CSS

--- gulpfile.js
+++ gulpfile.js
@@ -55,11 +55,7 @@ gulp.task('tdd', function (done) {
 // Styles
 gulp.task('styles', function() {
   return gulp.src(source + 'sass/pwp-*.scss')
-
-
-    .pipe(sass().on('error', sass.logError))
-
-
+    .pipe(sass({sourceComments: true}).on('error', sass.logError))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest(dest + 'css'))
     .pipe(rename({ suffix: '.min' }))
@@ -178,4 +174,3 @@ gulp.task('watch', function() {
 
 // Serve
 gulp.task('serve', ['build', 'watch']);
-
