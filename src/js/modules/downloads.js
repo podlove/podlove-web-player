4df0641b068ccb043833e6b92d9c2bfc35277239
Juri Leino <github@line-o.de>
2015-11-24 22:05:34 +0100

    feat (download): input field shows url
    The input field in the footer now shows the initial URL of the first
    asset in the select element.
    Whenever another asset is selected the URL in the input field changes.

--- src/js/modules/downloads.js
+++ src/js/modules/downloads.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var Tab = require('../tab');
-var linkInput;
 
 /**
  * Calculate the filesize into KB and MB
@@ -118,18 +117,32 @@ Downloads.prototype.createDownloadTab = function (params) {
     '</div>'
   );
   downloadTab.box.append($tabContent);
+
   var $button = $tabContent.find('button.pwp-download');
   var $select = $tabContent.find('select.select');
+
   $button.on('click', function (e) {
     e.preventDefault();
     window.open($select.val(), '_self');
   });
-  linkInput = $('<h3>Direkter Link</h3>' +
-    '<input type="url" name="download-link-url" readonly>');
-  linkInput.update = function(data) {
-    this.val(data.rawUrl);
-  };
-  downloadTab.createFooter('').append(linkInput);
+
+  // Add direct download URL for display to the user
+  // to footer of this tab
+  var $downloadLinkElement = $('<input name="download-link-url" type="url" readonly>');
+
+  function setUrl () {
+    $downloadLinkElement.val($select.val());
+  }
+
+  // set initial value
+  setUrl();
+
+  // change url whenever the user selects an asset
+  $select.on('change', setUrl);
+
+  downloadTab
+    .createFooter('<h3>Direkter Link</h3>')
+    .append($downloadLinkElement);
 
   return downloadTab;
 };
