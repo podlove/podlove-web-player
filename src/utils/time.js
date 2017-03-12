1b918cc60cce327d2615884dfd3a8dec4cfc4bd5
Alexander Heimbuch <github@heimbu.ch>
2017-03-12 20:53:11 +0100

    add avajs

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,9 +1,14 @@
+import compose from 'lodash/fp/compose'
+
+const enforceNumber = input => isNaN(parseInt(input)) ? 0 : input
 
 // Transforms a h:mm:ss or mm:ss or ss time to seconds
-export function timeToSeconds (time = '0') {
+export function timeToSeconds (time) {
+  time = time || '0'
   const partials = time.toString().split(':').reverse()
 
-  return partials.reduce((prev, curr, i) => prev + curr * Math.pow(60, i), 0)
+  return partials.reduce((prev, curr, i) =>
+    enforceNumber(prev) + enforceNumber(curr) * Math.pow(60, i), 0)
 }
 
 const calcSeconds = (time = 0) => parseInt(time % 60)
@@ -13,9 +18,9 @@ const leadingZero = (time) => time > 9 ? `${time}` : `0${time}`
 
 // Transforms seconds to (hh:)mm:ss
 export function secondsToTime (time = 0) {
-  let hours = calcHours(time)
-  let minutes = calcMinutes(time)
-  let seconds = calcSeconds(time)
+  let hours = compose(calcHours, enforceNumber)(time)
+  let minutes = compose(calcMinutes, enforceNumber)(time)
+  let seconds = compose(calcSeconds, enforceNumber)(time)
 
   let result = `${leadingZero(minutes)}:${leadingZero(seconds)}`
 
