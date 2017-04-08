e72f1f563d73129ee6ef39a31e17953f05c11fed
Alexander Heimbuch <github@heimbu.ch>
2017-04-08 15:15:03 +0200

    feature(deployment): Deploy assets to GitHub releases

---
+++ scripts/deploy-cdn.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Publishing to CDN"
+
+minorVersion=$(node ./scripts/version.js)
+user="podlove"
+server="cdn.podlove.org"
+port="10022"
+target="/home/podlove/projects/podlove-web-player"
+
+if [ -z "$1" ]
+  then
+    target=$target/$minorVersion
+  else
+    target=$target/$1
+fi
+
+echo "Creating $target@$server"
+ssh -o PasswordAuthentication=no -p $port $user@$server "mkdir -p $target"
+
+echo "Copy files to $target@$server"
+scp -o PasswordAuthentication=no -P $port -r dist/share.html dist/*.js $user@$server:$target
