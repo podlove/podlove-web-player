d309623ca0e308998e6bdcec230be7f2b306a5da
Alexander Heimbuch <github@heimbu.ch>
2017-04-28 10:54:17 +0200

    fix(babelrc): Fix invalid runtime configuration

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -88,7 +88,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
+      sourceMap: true,
       compress: {
         warnings: false
       }
