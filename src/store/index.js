1c427c4c01cae8a926ebebbb52e3a2b014131e70
Alexander Heimbuch <github@heimbu.ch>
2018-01-04 08:20:52 +0100

    fix(vendor-block): Remove error monitoring to prevent blocking of vendor bundle from iOS and Safari

--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,13 @@ import Vue from 'vue'
 import Revue from 'revue'
 import { createStore, applyMiddleware, compose } from 'redux'
 
-import Raven from 'raven-js'
-import createRavenMiddleware from 'raven-for-redux'
-
-import { sentry, version } from '../../package.json'
 import reducers from './reducers'
 import actions from './actions'
 import effects from './effects'
 
-Raven.config(sentry, { release: version, ignoreUrls: ['localhost'] }).install()
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const reduxStore = createStore(reducers, composeEnhancers(
-    applyMiddleware(createRavenMiddleware(Raven), effects)
-))
+const reduxStore = createStore(reducers, composeEnhancers(applyMiddleware(effects)))
 
 const store = new Revue(Vue, reduxStore, actions)
 
