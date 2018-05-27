f0a783f7c0a9a70d688814f6e3cd034ba8432f5c
Alexander Heimbuch <github@heimbu.ch>
2018-05-27 08:05:14 +0200

    tests(url-parameters): Adds integration tests for url parameters

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,6 +12,19 @@ Cypress.Commands.add('bootstrap', () => {
   cy.fixture('runtime').as('runtime')
 })
 
+Cypress.Commands.add('embed', (params = {}) => {
+  const query = Object.keys(params).reduce((result, key) => [...result, `${key}=${params[key]}`], []).join('&')
+  cy.visit(`/embed.html${query ? '?' + query : ''}`)
+})
+
+Cypress.Commands.add('iframe', {
+  prevSubject: 'element'
+}, $iframe => {
+  return new Cypress.Promise(resolve => {
+    resolve($iframe.contents().find('body'))
+  })
+})
+
 Cypress.Commands.add('play', () => {
   const selectors = domSelectors(cy)
   selectors.controls.playButton.button().then(btn => {
