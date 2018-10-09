0d022c9e464df2960de83900da4b6713cb9256b3
Alexander Heimbuch <github@heimbu.ch>
2018-10-09 18:30:12 +0200

    fix(bundle): Use bundled version of audio-driver

--- src/media/index.js
+++ src/media/index.js
@@ -1,5 +1,5 @@
-import { audio, events as audioEvents, actions as audioActions } from '@podlove/html5-audio-driver/src'
-import { attatchStream } from '@podlove/html5-audio-driver/src/hls'
+import { audio, events as audioEvents, actions as audioActions } from '@podlove/html5-audio-driver'
+import { attatchStream } from '@podlove/html5-audio-driver/hls'
 
 export default (audioFiles) => {
   const audioElement = attatchStream(audio(audioFiles))
