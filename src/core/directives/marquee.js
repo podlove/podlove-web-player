61b1a0c24b446b0596998160432cdfb859ce59b6
Alexander Heimbuch <github@heimbu.ch>
2018-11-10 14:16:57 +0100

    chore(marquee): fixes scrollbar on small viewport

--- src/core/directives/marquee.js
+++ src/core/directives/marquee.js
@@ -6,7 +6,8 @@ const marquee = el => {
   const animationDuration = scroller.scrollWidth / 50
 
   setStyles({
-    'white-space': 'nowrap'
+    'white-space': 'nowrap',
+    'overflow-x': 'hidden'
   })(scroller)
 
   setStyles({
@@ -19,24 +20,22 @@ const marquee = el => {
   })(scroller)
 
   setStyles({
-    'overflow': 'auto'
-  })(scroller)
-
-  if (scroller.scrollWidth > el.offsetWidth) {
-    addClasses('marquee-container')(el)
-    addClasses('marquee')(scroller)
-    setStyles({
-      'animation-duration': `${animationDuration > 10 ? animationDuration : 10}s`, // min 10s
-      width: `${scroller.scrollWidth}px`
-    })(scroller)
-  } else {
-    removeClasses('marquee-container')(el)
-    removeClasses('marquee')(scroller)
-  }
-
-  setStyles({
     'overflow-x': 'visible'
   })(scroller)
+
+  setTimeout(() => {
+    if (scroller.scrollWidth > el.offsetWidth) {
+      addClasses('marquee-container')(el)
+      addClasses('marquee')(scroller)
+      setStyles({
+        'animation-duration': `${animationDuration > 10 ? animationDuration : 10}s`, // min 10s
+        width: `${scroller.scrollWidth}px`
+      })(scroller)
+    } else {
+      removeClasses('marquee-container')(el)
+      removeClasses('marquee')(scroller)
+    }
+  }, 0)
 }
 
 export default {
