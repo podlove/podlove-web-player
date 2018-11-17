cfc427c7b284b8a2dd775485e540ad77cc4db59b
Joel Gerhold <joel@gmx.de>
2018-11-18 00:13:30 +0100

    feat(tabs): change audio-tab icon to speaker symbol

--- src/components/icons/AudioIcon.vue
+++ src/components/icons/AudioIcon.vue
@@ -1,11 +1,13 @@
 <template>
-  <svg width="25" height="25" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-miterlimit="1.5">
-      <g fill="none" :stroke="color || 'currentColor'" stroke-width="2">
-          <path d="M6 9h7m13 0h16m0 14h-5m-13 0H6m36 14h-5m-13 0H6"/>
-          <circle cx="17.5" cy="9" r="4.5" stroke-linejoin="round" stroke-miterlimit="1.414"/>
-          <circle cx="33" cy="23" r="4.5" stroke-linejoin="round" stroke-miterlimit="1.414"/>
-          <circle cx="33" cy="37" r="4.5" stroke-linejoin="round" stroke-miterlimit="1.414"/>
+  <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24.8 21">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+      <g :stroke="color || 'currentColor'">
+        <path d="M5.5,7.5H2C1.2,7.5,0.5,8.2,0.5,9v3c0,0.8,0.7,1.5,1.5,1.5h3.5l7,7v-20L5.5,7.5L5.5,7.5z"/>
+        <path d="M20,2.5c2.2,1.9,3.5,4.8,3.5,8s-1.3,6.1-3.5,8"/>
+        <path d="M17.5,4.5c1.7,1.4,3,3.7,3,6c0,2.3-1.3,4.6-3,6"/>
+        <path d="M15.5,7c1.1,0.8,2,2,2,3.5s-0.9,2.7-2,3.5"/>
       </g>
+    </g>
   </svg>
 </template>
 
