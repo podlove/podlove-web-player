bf2c64b35981e828a3885a0f6af2b88351f23443
Joel Gerhold <joel@gmx.de>
2018-11-18 11:37:24 +0100

    fix(tabs): changed the audio icon to the unified tab icon style

--- src/components/icons/AudioIcon.vue
+++ src/components/icons/AudioIcon.vue
@@ -1,11 +1,11 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24.8 21">
-    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 50 50">
+    <g stroke="none" stroke-width="2" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
       <g :stroke="color || 'currentColor'">
-        <path d="M5.5,7.5H2C1.2,7.5,0.5,8.2,0.5,9v3c0,0.8,0.7,1.5,1.5,1.5h3.5l7,7v-20L5.5,7.5L5.5,7.5z"/>
-        <path d="M20,2.5c2.2,1.9,3.5,4.8,3.5,8s-1.3,6.1-3.5,8"/>
-        <path d="M17.5,4.5c1.7,1.4,3,3.7,3,6c0,2.3-1.3,4.6-3,6"/>
-        <path d="M15.5,7c1.1,0.8,2,2,2,3.5s-0.9,2.7-2,3.5"/>
+        <path d="M12,19 L5,19 C3.4,19 2,20.4 2,22 L2,28 C2,29.6 3.4,31 5,31 L12,31 L26,45 L26,5 L12,19 L12,19 Z"></path>
+        <path d="M41,9 C45.4,12.8 48,18.6 48,25 C48,31.4 45.4,37.2 41,41"></path>
+        <path d="M36,13 C39.4,15.8 42,20.4 42,25 C42,29.6 39.4,34.2 36,37"></path>
+        <path d="M32,18 C34.2,19.6 36,22 36,25 C36,28 34.2,30.4 32,32"></path>
       </g>
     </g>
   </svg>
