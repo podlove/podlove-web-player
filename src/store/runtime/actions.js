f7d689fc40c5d15f0ca18e970f65f34d279d7730
Alexander Heimbuch <github@heimbu.ch>
2018-11-30 17:33:20 +0100

    fix(runtime): merges initial configuration with runtime

--- src/store/runtime/actions.js
+++ src/store/runtime/actions.js
@@ -1,5 +1,6 @@
 import { createAction } from 'redux-actions'
 
-import { SET_LANGUAGE } from '../types'
+import { SET_LANGUAGE, SET_RUNTIME } from '../types'
 
 export const setLanguage = createAction(SET_LANGUAGE)
+export const setRuntime = createAction(SET_RUNTIME)
